043ff34c91bacfa00dc0c66903888674d18c8846
RohitGhorai <98656284+RohitGhorai@users.noreply.github.com>
2023-10-20 11:20:09 +0530

    changes directory

---
+++ src/assets2/index.js
@@ -0,0 +1,10 @@
+import HTML from "./html.png";
+import AWS from "./aws.png";
+import GITHUB from "./github.png";
+import JS from "./js.png";
+import MYSQL from "./mysql.png";
+import NODEJS from "./node-js.png";
+import CSS from "./css.png";
+import REACT from "./react.png";
+
+export { HTML, AWS, GITHUB, JS, MYSQL, NODEJS, CSS, REACT };
--- src/constant2/index.js
+++ src/constant2/index.js
@@ -1,3 +1,5 @@
+import { HTML, NODEJS, MYSQL, AWS, JS, CSS, REACT } from "../assets2/index";
+
 export const navLinks = [
   {
     id: "home",
@@ -25,42 +27,42 @@ export const exp = [
   {
     id: "1",
     title: "HTML",
-    src: "../src/assets2/html.png",
+    src: { HTML },
   },
   {
     id: "2",
     title: "CSS",
-    src: "../src/assets2/css.png",
+    src: { CSS },
   },
   {
     id: "3",
     title: "Javascript",
-    src: "../src/assets2/js.png",
+    src: { JS },
   },
   {
     id: "4",
     title: "React",
-    src: "../src/assets2/react.png",
+    src: { REACT },
   },
   {
     id: "5",
     title: "Github",
-    src: "../src/assets2/github.png",
+    src: { GITHUB },
   },
   {
     id: "6",
     title: "Node JS",
-    src: "../src/assets2/node-js.png",
+    src: { NODEJS },
   },
   {
     id: "7",
     title: "MySql",
-    src: "../src/assets2/mysql.png",
+    src: { MYSQL },
   },
   {
     id: "8",
     title: "AWS",
-    src: "../src/assets2/aws.png",
+    src: { AWS },
   },
 ];
 
